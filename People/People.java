2a877187671059425bc9949f267ae2b44facd432
vetalEducation <tyu96@ukr.net>
2021-05-07 12:46:17 +0300

    Add functions peoplefunctions

--- People/People.java
+++ People/People.java
@@ -1,3 +1,23 @@
 public class People {
+	private PeopleFunctions functions;
 	
+	public People {
+		functions = new PeopleFunctions();
+	}
+	
+	public void moveBack() {
+		functions.moveBack();
+	}
+	
+	public void moveForward() {
+		functions.moveForward();
+	}
+	
+	public void moveLeft() {
+		functions.moveLeft();
+	}
+	
+	public void moveRight() {
+		functions.moveRight();
+	}
 }
