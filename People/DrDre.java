cbd1aa6df46dbb1ee8be2fe2af6ed0ef4681a08c
vetalEducation <tyu96@ukr.net>
2021-05-14 13:42:15 +0300

    Add functions DrDree.java

---
+++ People/DrDre.java
@@ -0,0 +1,8 @@
+public class Eminem extends People {
+	public final long IDcode = (long) (Math.random * 400_000_000);
+	
+	@Override
+	public String toString() {
+		return "John {" + "IDcode = " + IDcode + ", surname = " + surname + '}';
+	}
+}
