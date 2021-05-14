21b9d8d4fb74e62e4e1537275c82e523898110c5
vetalEducation <tyu96@ukr.net>
2021-05-14 13:49:51 +0300

    Refactor TestPeople

--- TestPeople.java
+++ TestPeople.java
@@ -1,6 +1,6 @@
 public class TestPeople {
 	public static void main(String[] args) {
-		John john = new John("Smith");
+		John john = new John("Lennon");
 		Liam liam = new Liam("Howlett");
 		
 		john.moveBack();
