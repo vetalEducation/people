d068f8f051e0db1fbc78bf00dd52e6f764feab69
vetalEducation <tyu96@ukr.net>
2021-05-14 14:10:02 +0300

    Refactor DrDree

--- People/DrDre.java
+++ People/DrDre.java
@@ -1,8 +1,8 @@
-public class Eminem extends People {
+public class DrDre extends People {
 	public final long IDcode = (long) (Math.random * 400_000_000);
 	
 	@Override
 	public String toString() {
-		return "John {" + "IDcode = " + IDcode'}';
+		return "DrDre {" + "IDcode = " + IDcode'}';
 	}
 }
